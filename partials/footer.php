bfac0f622d82892099985dc9bc8ed6d7c54df337
jovanie21 <desquitadojovanie21.com>
2022-12-13 02:45:43 +0800

    first commit

---
+++ partials/footer.php
@@ -0,0 +1,36 @@
+<!-- jQuery -->
+<script src="../styles/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../styles/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="../styles/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="../styles/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="../styles/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="../styles/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="../styles/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="../styles/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="../styles/plugins/moment/moment.min.js"></script>
+<script src="../styles/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../styles/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="../styles/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../styles/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../styles/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../styles/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="../styles/dist/js/pages/dashboard.js"></script>
+</body>
+</html>
